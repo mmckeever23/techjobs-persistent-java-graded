7d2245fb200abd64d10bc5acd317945a3efc82a1
Matt McKeever <mmckeever23@gmail.com>
2022-11-03 20:29:58 -0500

    Part 1 Completed

--- queries.sql
+++ queries.sql
@@ -1,7 +1,13 @@
 -- Part 1: Test it with SQL
+--id int PK
+--employer varchar(255)
+--name varchar(255)
+--skills varchar(255)
 
 -- Part 2: Test it with SQL
 
+
 -- Part 3: Test it with SQL
+
 
 -- Part 4: Test it with SQL
